13bace6798f2b37b803a480a49fdd9a7a06a3559
D1NNORT <71079416+D1NNORT@users.noreply.github.com>
2021-04-27 00:56:55 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -56,11 +56,7 @@ def equação_de_2º_grau():
         delta = pow(b, 2) - (4*a*c)
         if delta < 0:
             print("\033[1;32mNão existe raiz!\033[m")
-            cont = str(input("Deseja realizar outra equação?[S/N]:")).upper()
-            if cont == "S":
-                continue
-            else:
-                break
+            break
 
         x = (-b) + sqrt(delta) / 2 * a
         x2 = (-b) - sqrt(delta) / 2 * a
@@ -72,13 +68,14 @@ def equação_de_2º_grau():
 
         if delta == 0:
             print(f"\033[1;32mRaiz unica: {raiz1}\033[m")
+            break
 
         elif delta > 0:
             print(f"\033[1;32mAs duas raizes A e B são respectivamente: {raiz1} e {raiz2}\033[m")
-
-        cont = str(input("Deseja realizar outra equação?[S/N]:")).upper()
-        if cont == "N":
             break
+            
+    
+
 
 # loop
 while True:
